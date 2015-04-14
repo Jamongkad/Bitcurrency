a38a1ab3fc997ba157d777aad3458b32eacbe0d8
jamongkad <wrm932@gmail.com>
2015-04-14 13:13:52 +0800

    Initial Commit

---
+++ Bitcurrency/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Bitcurrency
+//
+//  Created by Mathew Wong on 4/14/15.
+//  Copyright (c) 2015 YidgetSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
